7d58b816b3315e4e9bfbba88bab99ed2dcaf553f
ziyadovea <ziyadovea@gmail.com>
2022-01-30 02:13:23 +0300

    added config file :wrench:

--- config/config.go
+++ config/config.go
@@ -1,1 +1,10 @@
 package config
+
+import "github.com/spf13/viper"
+
+func InitConfig() error {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("config") // path to look for the config file in
+	return viper.ReadInConfig()   // Find and read the config file
+}
